4d3e731dc340d869012c77313d8f4cf9efb9ef4d
jishan <imranislamjishan80@gmail.com>
2024-06-07 12:56:47 +0600

    quite show on text and prev next button impliment

---
+++ app/src/main/java/com/example/banglastoryapp/QuoteUtils.java
@@ -0,0 +1,14 @@
+package com.example.banglastoryapp;
+
+public class QuoteUtils {
+
+    public static String[] getQuotes1() {
+        return new String[] {
+                "Quote 1",
+                "Quote 2",
+                "Quote 3"
+        };
+    }
+
+
+}
--- app/src/main/java/com/example/banglastoryapp/StoryDetailsActivity.java
+++ app/src/main/java/com/example/banglastoryapp/StoryDetailsActivity.java
@@ -3,12 +3,57 @@ package com.example.banglastoryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StoryDetailsActivity extends AppCompatActivity {
+
+    Button btnPrev, btnNext;
+    TextView tvQuite, tvCount;
+
+    private String[] quotes;
+    private int currentIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_details);
+
+        btnPrev = findViewById(R.id.btnPrev);
+        btnNext = findViewById(R.id.btnNext);
+        tvQuite = findViewById(R.id.tvQuite);
+        tvCount = findViewById(R.id.tvCount);
+
+
+        quotes = QuoteUtils.getQuotes1();
+
+        // Display the first quote initially
+        tvQuite.setText(quotes[currentIndex]);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentIndex > 0) {
+                    currentIndex--;
+                    tvQuite.setText(quotes[currentIndex]);
+                }
+            }
+        });
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentIndex < quotes.length - 1) {
+                    currentIndex++;
+                    tvQuite.setText(quotes[currentIndex]);
+                }
+            }
+        });
+
+
+
     }
 }

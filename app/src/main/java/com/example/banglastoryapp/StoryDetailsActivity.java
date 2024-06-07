46febe4a38935028ec7c406337374f5eae0ad8e7
jishan <imranislamjishan80@gmail.com>
2024-06-07 17:07:58 +0600

    story page count show on textview

--- app/src/main/java/com/example/banglastoryapp/StoryDetailsActivity.java
+++ app/src/main/java/com/example/banglastoryapp/StoryDetailsActivity.java
@@ -15,7 +15,7 @@ public class StoryDetailsActivity extends AppCompatActivity {
 
     private String[] quotes;
     private int currentIndex = 0;
-
+    public static int categoryId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +29,34 @@ public class StoryDetailsActivity extends AppCompatActivity {
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("Story Details");
+            getSupportActionBar().setTitle("Story");
         }
 
-        quotes = QuoteUtils.getQuotes1();
+        switch (categoryId){
+            case 0:
+                quotes = QuoteUtils.getQuotes0();
+                break;
+            case 1:
+                quotes = QuoteUtils.getQuotes1();
+                break;
+            case 2:
+                quotes = QuoteUtils.getQuotes2();
+                break;
+            case 3:
+                quotes = QuoteUtils.getQuotes3();
+                break;
+            case 4:
+                quotes = QuoteUtils.getQuotes4();
+                break;
+            case 5:
+                quotes = QuoteUtils.getQuotes5();
+                break;
+        }
+
 
         // Display the first quote initially
         tvQuite.setText(quotes[currentIndex]);
+        tvCount.setText(""+(currentIndex+1));
 
         btnPrev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +64,7 @@ public class StoryDetailsActivity extends AppCompatActivity {
                 if (currentIndex > 0) {
                     currentIndex--;
                     tvQuite.setText(quotes[currentIndex]);
+                    tvCount.setText(""+(currentIndex+1) );
                 }
             }
         });
@@ -54,9 +76,11 @@ public class StoryDetailsActivity extends AppCompatActivity {
                 if (currentIndex < quotes.length - 1) {
                     currentIndex++;
                     tvQuite.setText(quotes[currentIndex]);
+                    tvCount.setText(""+(currentIndex+1) );
                 }
             }
         });
+
 
 
 

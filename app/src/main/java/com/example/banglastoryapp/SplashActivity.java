953ca8d2f2036842de5a539115f4c0cc1e9b5623
jishan <imranislamjishan80@gmail.com>
2024-06-07 13:02:13 +0600

    spash screen complte

---
+++ app/src/main/java/com/example/banglastoryapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.banglastoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+
+
+    }
+}
